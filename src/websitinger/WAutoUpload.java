71b5b238659f2fcdc706ecf3658a95ec2e728a64
RobotMan2412 <julian@scheffers.net>
2021-05-30 19:57:07 +0200

    Stubby.

--- src/websitinger/WAutoUpload.java
+++ src/websitinger/WAutoUpload.java
@@ -45,7 +45,7 @@ public class WAutoUpload {
 		}
 		
 		for (Map.Entry<String, String> dirPair : localToSiteDirs.entrySet()) {
-			File file = new File(dirPair.getKey());
+			File file = new File(dirPair.getKey());//
 			if (file.exists()) {
 				checkUpload(file, dirPair.getValue(), passwd, ssh, true, true);
 			}
