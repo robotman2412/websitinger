4c5ad1edc48f5ccc563c4593438dbb916cfbdaa1
RobotMan2412 <julian@scheffers.net>
2021-11-15 14:38:49 +0100

    remove old crap i backed up anyway

--- website_old/special/cpuc/stub_r3.py
+++
@@ -1,4 +0,0 @@
-
-# Stub file for GR8CPU Rev3.1 python.
-
-print("Hello, world!") # Hello, World!
--- website_old/special/gr8cpu/asmvalueincl.asm
+++
@@ -1,6 +0,0 @@
-
-		load #'.'
-		copy A to D
-		;there is a difference in the LOADs
-		load $00
-		store $fe
